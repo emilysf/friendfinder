0a53e7d7cf4c69962cde6bd9950e123b8c556ca5
emilysf <emilysfrazin@gmail.com>
2016-06-29 19:26:51 -0400

    server.js file setup local server connected

---
+++ server.js
@@ -0,0 +1,26 @@
+//npm packages
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//express setup/local port setup
+var app = express();
+var PORT = process.env.PORT || 8080; 
+
+//body-parser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('public'));
+
+//points server to the route files
+require('./routing/api-routes.js')(app); 
+require('./routing/html-routes.js')(app);
+
+//listener starts the server
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
+

84e3e807b0a5c2a302f852ffa162bf4d798b2244
emilysf <emilysfrazin@gmail.com>
2016-06-30 18:16:39 -0400

    javascript working/modals working

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -36,7 +36,8 @@ module.exports = function(app){
 
         allDiffs.length = 0;
 
-        response.send(true);
+        response.json(friends[bestMatch]);
+
 
 
         console.log("best match is " + bestMatch);
